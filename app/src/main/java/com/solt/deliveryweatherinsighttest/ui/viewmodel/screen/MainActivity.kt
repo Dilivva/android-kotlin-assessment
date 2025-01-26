4378b35955cdc93aa648cda0273fb026f20ae063
Eric Ibu <ericomartin.0785@gmail.com>
2025-01-26 14:56:19 +0100

    chore: issues with buidling the project. No gradle config

---
+++ app/src/main/java/com/solt/deliveryweatherinsighttest/ui/viewmodel/screen/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.solt.deliveryweatherinsighttest.ui.viewmodel.screen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+}
