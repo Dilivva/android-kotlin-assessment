406fdf40f51a77b57fb8e8798d8cef7e47313377
OYERINDE Enoch <57858245+EnochOluwadamilare@users.noreply.github.com>
2025-02-09 17:00:15 +0100

    Revert "Eric ibu submit"

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_basebox_weatherinsights_MyDataApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.basebox.weatherinsights.MyDataApp"
-)
-public final class _com_basebox_weatherinsights_MyDataApp {
-}
